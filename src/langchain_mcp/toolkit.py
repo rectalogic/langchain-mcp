a7f291d878988e7086da23960975fa339b417391
Andrew Wason <rectalogic@rectalogic.com>
2025-01-04 13:15:05 -0500

    Work around langchain changes to BaseTool schema
    It now requires the schema model actually define pydantic fields.
    
    Update version to 0.1.0
    
    Fixes #1

--- src/langchain_mcp/toolkit.py
+++ src/langchain_mcp/toolkit.py
@@ -49,10 +49,35 @@ class MCPToolkit(BaseToolkit):
         ]
 
 
+TYPEMAP = {
+    "integer": int,
+    "number": float,
+    "array": list,
+    "boolean": bool,
+    "string": str,
+    "null": type(None),
+}
+
+FIELD_DEFAULTS = {
+    int: 0,
+    float: 0.0,
+    list: [],
+    bool: False,
+    str: "",
+    type(None): None,
+}
+
+
+def configure_field(name: str, type_: dict[str, t.Any], required: list[str]) -> tuple[type, t.Any]:
+    field_type = TYPEMAP[type_["type"]]
+    default_ = FIELD_DEFAULTS.get(field_type) if name not in required else ...
+    return field_type, default_
+
+
 def create_schema_model(schema: dict[str, t.Any]) -> type[pydantic.BaseModel]:
     # Create a new model class that returns our JSON schema.
     # LangChain requires a BaseModel class.
-    class Schema(pydantic.BaseModel):
+    class SchemaBase(pydantic.BaseModel):
         model_config = pydantic.ConfigDict(extra="allow")
 
         @t.override
@@ -62,7 +87,14 @@ def create_schema_model(schema: dict[str, t.Any]) -> type[pydantic.BaseModel]:
         ) -> JsonSchemaValue:
             return schema
 
-    return Schema
+    # Since this langchain patch, we need to synthesize pydantic fields from the schema
+    # https://github.com/langchain-ai/langchain/commit/033ac417609297369eb0525794d8b48a425b8b33
+    required = schema.get("required", [])
+    fields: dict[str, t.Any] = {
+        name: configure_field(name, type_, required) for name, type_ in schema["properties"].items()
+    }
+
+    return pydantic.create_model("Schema", __base__=SchemaBase, **fields)
 
 
 class MCPTool(BaseTool):
